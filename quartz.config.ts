62c463dfdef9762a39a9938ca6815047c86f6e17
Will Buchner <william.p.buchner@gmail.com>
2026-04-12 19:31:46 -0700

    Quartz sync: Apr 12, 2026, 7:31 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -55,6 +55,7 @@ const config: QuartzConfig = {
   },
   plugins: {
     transformers: [
+      Plugin.HardLineBreaks(),
       Plugin.FrontMatter(),
       Plugin.CreatedModifiedDate({
         priority: ["frontmatter", "git", "filesystem"],
